1bfb8ddbf320a43113b82446e25c15b158333e09
lorita81 <125750244+lorita81@users.noreply.github.com>
2024-01-28 13:45:35 +0000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+ function App(){
+  return(
+    <h1>
+      davaleba-2
+    </h1>
+
+  )
+ }
+
+const root = document.getElementById('root')
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
